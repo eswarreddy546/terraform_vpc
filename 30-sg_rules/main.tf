69068c13221ebac758d458d2e8a2e3699e1315dc
Asri <asri>
2026-03-06 11:30:13 +0530

    terraform modules

--- 30-sg_rules/main.tf
+++ 30-sg_rules/main.tf
@@ -18,8 +18,16 @@ resource "aws_security_group_rule" "mongodb" {
   to_port           = 22
   
 }
+resource "aws_security_group_rule" "mongodb_egress" {
+  type              = "egress"
+  security_group_id = local.mongodb_security_group_id
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
 
-#redis ingress and egress
+# redis ingress and egress
 
 resource "aws_security_group_rule" "redis" {
   type              = "ingress"
@@ -62,52 +70,61 @@ resource "aws_security_group_rule" "r_egress" {
   cidr_blocks       = ["0.0.0.0/0"]
 }
 
-resource "aws_security_group_rule" "mysql" {
-  type              = "ingress"
-  security_group_id = local.mysql_security_group_id
-  source_security_group_id = local.bastion_sg_id
-  from_port         = 22
-  protocol          = "tcp"
-  to_port           = 22
+# resource "aws_security_group_rule" "mysql" {
+#   type              = "ingress"
+#   security_group_id = local.mysql_security_group_id
+#   source_security_group_id = local.bastion_sg_id
+#   from_port         = 22
+#   protocol          = "tcp"
+#   to_port           = 22
   
-}
+# }
 
-resource "aws_security_group_rule" "mysql_egress" {
-  type              = "egress"
-  security_group_id = local.mysql_security_group_id
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_security_group_rule" "mysql_egress" {
+#   type              = "egress"
+#   security_group_id = local.mysql_security_group_id
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
 
-#catalogue
+# #catalogue
 
-resource "aws_security_group_rule" "catalogue" {
-  type              = "ingress"
-  security_group_id = local.catalogue_security_group_id
-  source_security_group_id = local.bastion_sg_id
-  from_port         = 22
-  protocol          = "tcp"
-  to_port           = 22
+# resource "aws_security_group_rule" "catalogue" {
+#   type              = "ingress"
+#   security_group_id = local.catalogue_security_group_id
+#   source_security_group_id = local.bastion_sg_id
+#   from_port         = 22
+#   protocol          = "tcp"
+#   to_port           = 22
   
-}
+# }
 
-resource "aws_security_group_rule" "catalogue_egress" {
-  type              = "egress"
-  security_group_id = local.mysql_security_group_id
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-}
+# resource "aws_security_group_rule" "catalogue_egress" {
+#   type              = "egress"
+#   security_group_id = local.mysql_security_group_id
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+# }
 
-resource "aws_security_group_rule" "mongodb_catalogue" {
-  type              = "ingress"
-  security_group_id = local.mongodb_security_group_id
-  source_security_group_id = local.mongodb_security_group_id
-  from_port         = 27017
-  protocol          = "tcp"
-  to_port           = 27017
-}
+# resource "aws_security_group_rule" "mongodb_catalogue" {
+#   type              = "ingress"
+#   security_group_id = local.mongodb_security_group_id
+#   source_security_group_id = local.mongodb_security_group_id
+#   from_port         = 27017
+#   protocol          = "tcp"
+#   to_port           = 27017
+# }
+
+# resource "aws_security_group_rule" "catalogue_backend_alb" {
+#   type              = "ingress"
+#   security_group_id = local.catalogue_security_group_id
+#   source_security_group_id = local.backend_alb_sg_id
+#   from_port         = 8080
+#   protocol          = "tcp"
+#   to_port           = 8080
+# }
